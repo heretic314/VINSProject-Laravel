4e1130db4312f33a4622ee186f4cd08afdd9ebf0
masterpanda33 <ayushg.swatcat@gmail.com>
2018-01-16 14:42:59 +0530

    LabSheet

--- resources/views/LabSheet.blade.php
+++ resources/views/LabSheet.blade.php
@@ -27,52 +27,7 @@
 		{{ csrf_field() }}
 
 		<div class="row form-group">
-			<div class="col-md-10"><nav class="navbar navbar-default navbar-fixed-top">
-	<div class="container-fluid">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-			<a class="navbar-brand" href="#">VinsLogo</a>
-			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsed_menu" aria-expanded="false">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-		</div>
-
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="collapsed_menu">
-			<ul class="nav navbar-nav">
-				<li><a href="#">Home</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">List of Forms <span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Form1</a></li>
-						<li><a href="#">Form2</a></li>
-						<li><a href="#">Form3</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="#">Form4</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="#">Form5</a></li>
-					</ul>
-				</li>
-			</ul>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Link</a></li>
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Dropdown <span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="#">Separated link</a></li>
-					</ul>
-				</li>
-			</ul>
-		</div><!-- /.navbar-collapse -->
-
-	</div><!-- /.container-fluid -->
-	</nav>
+			<div class="col-md-10">
 				<div class="row">
 					<div class="col-md-6">
 						<div class="col-md-6">
