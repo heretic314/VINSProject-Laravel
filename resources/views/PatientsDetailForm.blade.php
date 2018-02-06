43399b838516a75ea3a8ccc4591dbe661372cc3a
Stuti <gargstutee@gmail.com>
2018-02-06 14:25:53 +0530

    Patient diet and patient detail
    url error

--- resources/views/PatientsDetailForm.blade.php
+++ resources/views/PatientsDetailForm.blade.php
@@ -22,7 +22,7 @@
 		@endif
 
 
-		<form action="/PatientsDetailForm" method = "post">
+		<form action="{{ url"/PatientsDetailForm" }} method = "post">
 			{{ csrf_field() }}
 
 			<div class="row form-group">
@@ -133,7 +133,7 @@
 			      <label for="address">Address: </label>
 					</div>
 					<div class="col-md-6">
-			      <textarea class="form-control" type="text" id="address" name="address" > 
+			      <textarea class="form-control" type="text" id="address" name="address" >
 							 {{old('address')}}
 						@if($errors->has('address'))
 								<span class="help-block"><div class="text-danger">{{ $errors->first('address') }}</div></span>
